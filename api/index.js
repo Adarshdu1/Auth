4db5e6fc195653554443358f6a7be7bb3969917d
HAKD@1 <dubeyad2003@gmail.com>
2023-08-19 11:51:43 +0530

    created signup api route and managed authentication

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongodb from "mongodb";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -13,9 +14,12 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
 
 app.use("/api/user", userRoutes);
+
+app.use("/api/auth", authRoutes);
