b59a7749a51b9b76aed5eef206fbb7ab029fb430
HAKD@1 <dubeyad2003@gmail.com>
2023-08-20 12:45:43 +0530

    Completed Sign In UI and added sign in functionality.

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -20,6 +21,7 @@ export default function SignUp() {
       const data = await res.json();
       if (!res.ok) throw Error(data.message);
       setLoading(false);
+      navigate("/sign-in");
     } catch (error) {
       console.log(error);
       setLoading(false);
