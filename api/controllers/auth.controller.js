064c5232ed3a58f1023d9e0fc58eca1fff47153e
HAKD@1 <dubeyad2003@gmail.com>
2023-08-20 12:28:38 +0530

    Created sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -13,5 +14,25 @@ export const signup = async (req, res, next) => {
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     next(errorHandler(300, "User already exists"));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) throw errorHandler(404, "User not found");
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) throw errorHandler(401, "Wrong Credentials");
+    const token = jwt.sign({ id: validUser._id }, process.env.SECRET, {
+      expiresIn: 3600,
+    });
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
